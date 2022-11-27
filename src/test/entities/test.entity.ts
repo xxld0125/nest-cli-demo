a3d187b0b7d58ff5048c2382cdf8926219287858
xulf01 <xulf01@mingyuanyun.com>
2022-11-27 19:39:35 +0800

    @feat: 创建映射到数据库表的类

--- src/test/entities/test.entity.ts
+++ src/test/entities/test.entity.ts
@@ -1,13 +1,44 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Generated,
+  BeforeInsert,
+} from 'typeorm';
 
 @Entity()
 export class Test {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  @Column()
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  @Column()
-  age: number;
+  @Column({
+    select: true,
+    comment: '注释',
+    default: '123',
+    nullable: true,
+  })
+  password: string;
+
+  @Generated('uuid')
+  uuid: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createTime: Date;
+
+  @Column({
+    type: 'enum',
+    enum: [1, 2, 3],
+    default: 1,
+  })
+  list: number;
+
+  @Column('simple-array')
+  names: string[];
+
+  @Column('simple-json')
+  json: { name: string; value: number };
 }
