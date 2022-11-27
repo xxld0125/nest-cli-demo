531b8888e2d6956506a0c7c168c213bf2d1a2818
xulf01 <xulf01@mingyuanyun.com>
2022-11-27 16:44:54 +0800

    @feat: swager

--- src/guard/dto/create-guard.dto.ts
+++ src/guard/dto/create-guard.dto.ts
@@ -1,1 +1,12 @@
-export class CreateGuardDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateGuardDto {
+  @ApiProperty({
+    example: '张三',
+  })
+  name: string;
+  @ApiProperty({
+    example: 20,
+  })
+  age: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { HttpFilter } from './common/filter';
 import { ValidationPipe } from '@nestjs/common';
 
 // import { GuardGuard } from './guard/guard.guard';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const whiteList = ['/list'];
 
 function middleWareAll(req, res, next) {
@@ -20,6 +22,16 @@ function middleWareAll(req, res, next) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger 接口文档配置
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('nest-cli-demo')
+    .setDescription('测试项目')
+    .setVersion('1')
+    .build();
+  const ducument = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api-docs', app, ducument);
 
   app.use(
     session({
